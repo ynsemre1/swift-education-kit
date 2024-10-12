d7f21ceef6f909263a7e0f1c6b43cd13e1965ec7
Yunus Emre <kahraman03040506@hotmail.com>
2024-10-12 06:35:43 +0300

    The I am Rich App

---
+++ I am Rich/I am RichTests/I_am_RichTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichTests.swift
+//  I am RichTests
+//
+//  Created by Yunus Emre AKYOL on 30.09.2024.
+//
+
+import Testing
+@testable import I_am_Rich
+
+struct I_am_RichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
