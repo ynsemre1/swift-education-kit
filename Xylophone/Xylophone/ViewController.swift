2ae65750867cd09c65bdfdd0d365a2d50bcc4fae
Yunus Emre <kahraman03040506@hotmail.com>
2024-10-28 04:16:44 +0300

    Xylophone Completed

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(senderName: sender.currentTitle!)
+        colorOpacity(senderName: sender)
+    }
     
-
+    // func funcName() { do stuff }
+    
+    func playSound(senderName: String) {
+        let url = Bundle.main.url(forResource: senderName, withExtension: "wav")!
+        player = try! AVAudioPlayer(contentsOf: url)
+        player?.play()
+    }
+    
+    func colorOpacity(senderName: UIButton){
+        senderName.alpha = 0.5
+        UIView.animate(withDuration: 0.4) {
+            senderName.alpha = 1
+        }
+    }
 }
-
