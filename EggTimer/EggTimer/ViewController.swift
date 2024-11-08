164501e08efab2f0d6a5b76056682df52f8fdc74
Yunus Emre <kahraman03040506@hotmail.com>
2024-11-08 06:11:30 +0300

    EggTimer Completed

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -11,35 +11,40 @@ import UIKit
 class ViewController: UIViewController {
     
     var countdownTimer: Timer?
-        
-        let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
-        
-        @IBOutlet weak var titleLabel: UILabel!
     
-        @IBAction func hardnessSelected(_ sender: UIButton) {
-            let hardness = sender.currentTitle!
-            
-            // Retrieve the countdown seconds based on the selected hardness level
-            if let countdownSeconds = eggTimes[hardness] {
-                startCountdown(countdownSeconds)
-            } else {
-                print("Invalid selection.")
-            }
-        }
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
         
-        func startCountdown(_ countdownSeconds: Int) {
-            var remainingSeconds = countdownSeconds // Local variable to track remaining seconds
-            
-            countdownTimer?.invalidate() // Stop any existing timer
-
-            countdownTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-                if remainingSeconds > 0 {
-                    print(remainingSeconds) // Print each second
-                    remainingSeconds -= 1
-                } else {
-                    timer.invalidate() // Stop the countdown when it reaches zero
-                    self.titleLabel.text = "Done!.."
-                }
+        // Retrieve the countdown seconds based on the selected hardness level
+        if let countdownSeconds = eggTimes[hardness] {
+            startCountdown(countdownSeconds)
+        } else {
+            print("Invalid selection.")
+        }
+    }
+    
+    func startCountdown(_ countdownSeconds: Int) {
+        var remainingSeconds = countdownSeconds // Local variable to track remaining seconds
+        
+        countdownTimer?.invalidate() // Stop any existing timer
+        
+        countdownTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            if remainingSeconds > 0 {
+                print(remainingSeconds) // Print each second
+                remainingSeconds -= 1
+                // Calculate progress as a Float value between 0.0 and 1.0
+                let progress = Float(countdownSeconds - remainingSeconds) / Float(countdownSeconds)
+                self.progressBar.progress = progress // Set the progress
+            } else {
+                timer.invalidate() // Stop the countdown when it reaches zero
+                self.titleLabel.text = "Done!.."
+                self.progressBar.progress = 0.0 // Set the progress
             }
         }
+    }
 }
