3e7b63540fc8a2b9c98a7e02a005f5d97881c2fb
Yunus Emre <kahraman03040506@hotmail.com>
2024-11-08 05:59:09 +0300

    Timer Added

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var countdownTimer: Timer?
+        
+        let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+        
+        @IBOutlet weak var titleLabel: UILabel!
+    
+        @IBAction func hardnessSelected(_ sender: UIButton) {
+            let hardness = sender.currentTitle!
+            
+            // Retrieve the countdown seconds based on the selected hardness level
+            if let countdownSeconds = eggTimes[hardness] {
+                startCountdown(countdownSeconds)
+            } else {
+                print("Invalid selection.")
+            }
+        }
+        
+        func startCountdown(_ countdownSeconds: Int) {
+            var remainingSeconds = countdownSeconds // Local variable to track remaining seconds
+            
+            countdownTimer?.invalidate() // Stop any existing timer
 
-
+            countdownTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                if remainingSeconds > 0 {
+                    print(remainingSeconds) // Print each second
+                    remainingSeconds -= 1
+                } else {
+                    timer.invalidate() // Stop the countdown when it reaches zero
+                    self.titleLabel.text = "Done!.."
+                }
+            }
+        }
 }
