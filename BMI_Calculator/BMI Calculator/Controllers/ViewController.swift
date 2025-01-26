e774f48db1ddc194885c8a0980be0ee497ee95ac
Yunus Emre <kahraman03040506@hotmail.com>
2025-01-26 08:23:36 +0300

    Calculator Func Completed

--- BMI_Calculator/BMI Calculator/Controllers/ViewController.swift
+++ BMI_Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightText: UILabel!
+    @IBOutlet weak var weightText: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightText.text = String(format: "%.2f", sender.value) + "m"
+    }
+    
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightText.text = String(Int(sender.value)) + "Kg"
+    }
+    
+    func calculateBMI() -> Double {
+        let height = Double(heightSlider.value)
+        let weight = Double(weightSlider.value)
+        
+        let bmi = weight / (height * height)
+        
+        return bmi
+    }
+    
+    @IBAction func calculateButton(_ sender: UIButton) {
+        calculateBMI()
+        
+    }
 }
 
