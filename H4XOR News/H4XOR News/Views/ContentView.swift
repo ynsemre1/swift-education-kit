64fff78520536f1201507915e3886e92bec740b9
Yunus Emre <kahraman03040506@hotmail.com>
2025-02-28 00:15:58 +0300

    Detail View Add

--- H4XOR News/H4XOR News/Views/ContentView.swift
+++ H4XOR News/H4XOR News/Views/ContentView.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+
     @ObservedObject var networkManager = NetworkManager()
-    
+
     var body: some View {
         NavigationView {
             List(networkManager.posts) { post in
-                HStack{
-                    Text(String(post.points))
-                    Text(post.title)
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
                 }
             }
             .navigationBarTitle("H4XOR News")
---
+++ H4XOR News/H4XOR News/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  H4XOR News
+//
+//  Created by Yunus Emre AKYOL on 28.02.2025.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    DetailView(url: "https://www.google.com")
+}
