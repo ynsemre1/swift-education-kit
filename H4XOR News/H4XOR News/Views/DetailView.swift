d041b96eeb27b834332f7a9bc809814471d50ebb
Yunus Emre <kahraman03040506@hotmail.com>
2025-03-02 06:47:08 +0300

    Detail View Add

--- H4XOR News/H4XOR News/Views/DetailView.swift
+++ H4XOR News/H4XOR News/Views/DetailView.swift
@@ -6,16 +6,19 @@
 //
 
 import SwiftUI
+import WebKit
+
 
 struct DetailView: View {
     
     let url: String?
     
     var body: some View {
-        Text("Hello World")
+        WebView(urlString: url)
     }
 }
 
 #Preview {
     DetailView(url: "https://www.google.com")
 }
+
---
+++ H4XOR News/H4XOR News/Views/WebView.swift
@@ -0,0 +1,27 @@
+//
+//  WebView.swift
+//  H4XOR News
+//
+//  Created by Yunus Emre AKYOL on 2.03.2025.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString, let url = URL(string: safeString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+}
+
